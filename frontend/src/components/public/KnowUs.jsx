eec9d53c0e88117ff0055071c52b1ddc6f024d54
pramodpandit72 <nwdpramod12305901@gmail.com>
2026-02-23 20:25:05 +0530

    Fix all case sensitivity issues in imports

--- frontend/src/components/public/KnowUs.jsx
+++ frontend/src/components/public/KnowUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../ui/Button';
+import Button from '../ui/button';
 import { Link } from 'react-router-dom';
 import footer_logo from "../../assets/footer_logo.png";
 
